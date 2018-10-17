6d70e5ad9bbb6f7007f06156e21af44dfa1ab710
pyld <lidongone@qq.com>
2018-10-18 02:27:43 +0800

    init files

---
+++ dist/app/__init__.py
@@ -0,0 +1,8 @@
+def main():
+    import bottle
+
+    print("Test bottle:", bottle.__version__)
+
+
+if __name__ == "__main__":
+    main()
---
+++ shiv_me.py
@@ -0,0 +1,24 @@
+import os
+import platform
+
+
+def main():
+    pl = platform.platform()
+    is_win = "windows" in pl.lower()
+    with open("runtime.txt") as f:
+        runtime = f.read().strip()
+    cmd = (
+        'shiv -o app.pyz -p "%s" -r requirements.txt --site-packages dist -e app:main'
+        % runtime
+    )
+    line = "=" * 50
+    suggest_runtime = "python3" if is_win else "/usr/bin/env python3"
+    print(
+        "%s\n\nPlatform:\n%s\n\nSuggest Runtime:\n%s\n\nRunning:\n%s\n\n%s"
+        % (line, pl, suggest_runtime, cmd, line)
+    )
+    os.system(cmd)
+
+
+if __name__ == "__main__":
+    main()
