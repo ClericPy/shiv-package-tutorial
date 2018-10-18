58ef30994e3aa39c7d6709b212ef2d6452a44255
pyld <lidongone@qq.com>
2018-10-18 21:40:35 +0800

    Update shiv_me.py

--- shiv_me.py
+++ shiv_me.py
@@ -7,12 +7,13 @@ def main():
     is_win = "windows" in pl.lower()
     with open("runtime.txt") as f:
         runtime = f.read().strip()
+    suggest_runtime = "python3" if is_win else "/usr/bin/env python3"
+    runtime = runtime or suggest_runtime
     cmd = (
         'shiv -o app.pyz -p "%s" -r requirements.txt --site-packages dist -e app:main'
         % runtime
     )
     line = "=" * 50
-    suggest_runtime = "python3" if is_win else "/usr/bin/env python3"
     print(
         "%s\n\nPlatform:\n%s\n\nSuggest Runtime:\n%s\n\nRunning:\n%s\n\n%s"
         % (line, pl, suggest_runtime, cmd, line)
